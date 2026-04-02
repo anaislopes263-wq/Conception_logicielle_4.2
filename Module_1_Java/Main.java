2049035749d42380031d8d649be29715b1a42e8f
Thomas DBD <thodub56@gmail.com>
2026-04-02 10:04:22 +0200

    TP1/TP2/TP3 MODULE_1_JAVA

--- Module_1_Java/Main.java
+++ Module_1_Java/Main.java
@@ -1,25 +1,40 @@
 import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Etudiant etudiant1 = new Etudiant();
-        etudiant1.nom = "Alice";
-        etudiant1.age = 20;
-        etudiant1.numeroEtudiant = "E12345";
-        etudiant1.moyenne = 15.5f;
-        etudiant1.filiere = "Informatique";
-        etudiant1.coursSuivis = new ArrayList<String>();
-        etudiant1.coursSuivis.add("Programmation");
-        etudiant1.coursSuivis.add("Algorithmes");
 
-        Cours cours1 = new Cours();
-        cours1.Nom = "Programmation";
-        cours1.Professeur = "M. Dupont";
+        ArrayList<Personne> personnes = new ArrayList<>();
 
-        System.out.println("Etudiant: " + etudiant1.nom);
-        System.out.println("Age: " + etudiant1.age);
-        System.out.println("Numero Etudiant: " + etudiant1.numeroEtudiant);
-        System.out.println("Moyenne: " + etudiant1.moyenne);
-        System.out.println("Filiere: " + etudiant1.filiere);
-        System.out.println("Cours Suivis: " + etudiant1.coursSuivis);
+        // Etudiant
+        Etudiant e1 = new Etudiant();
+        e1.setNom("Alice");
+        e1.setAge(20);
+        e1.numeroEtudiant = "E123";
+        e1.setMoyenne(15.5f);
+
+        // Enseignant
+        Enseignant prof = new Enseignant();
+        prof.setNom("Dupont");
+        prof.setAge(45);
+        prof.salaire = 3000;
+
+        // Etudiant
+        Etudiant e2 = new Etudiant();
+        e2.setNom("Bob");
+        e2.setAge(22);
+        e2.numeroEtudiant = "E456";
+        e2.setMoyenne(16.0f);
+
+        // Ajout
+        personnes.add(e1);
+        personnes.add(e2);
+        personnes.add(prof);
+        
+
+        // Polymorphisme
+        for (Personne p : personnes) {
+            p.afficher_details();
+            System.out.println("------------");
+        }
     }
 }
